19c14ad7ed577ccab4b8a4e38d7b7856658b274f
Paulo Cesar Carneiro <paulo_c_carneiro@hotmail.com>
2024-09-28 11:48:00 -0300

    blog.web2
    apagando projeto blow.web2

--- Blog.Web2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Blog.Web2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
