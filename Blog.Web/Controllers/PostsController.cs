d1a230786928db6649e7bb18ba8ce0db14a39031
Paulo Cesar Carneiro <paulo_c_carneiro@hotmail.com>
2024-10-10 23:05:27 -0300

    organizando o projeto web

---
+++ Blog.Web/Controllers/PostsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    [Route("meus-posts")]
+    public class PostsController : Controller
+    {
+        // GET: Posts
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //// GET: Posts/detalhes/5
+        [HttpGet("detalhes/{id:int}")]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //// GET: Posts/Create
+        [HttpGet("novo")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PostController1/Create
+        [HttpPost("novo")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PostController1/Edit/5
+        [HttpGet("editar/{id:int}")]
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PostController1/Edit/5
+        [HttpPost("editar/{id:int}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PostController1/Delete/5
+        [HttpGet("excluir/{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PostController1/Delete/5
+        [HttpPost("excluir/{id:int}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
