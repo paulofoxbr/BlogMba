d4cd0fc40fe3542bac9b58bb7ce2cbdbea9bf6f0
Paulo Cesar Carneiro <paulo_c_carneiro@hotmail.com>
2024-10-12 23:11:40 -0300

    incluindo mais serivços

--- Blog.Data/Services/PostService.cs
+++ Blog.Data/Services/PostService.cs
@@ -28,5 +28,27 @@ namespace Blog.Data.Services
         {
             return await _context.Posts.ToListAsync();
         }
+
+        public async Task<Post> GetPostByIdAsync(int id)
+        {
+            return await _context.Posts.FindAsync(id);
+        }
+
+        public async Task UpdatePostAsync(Post post)
+        {
+            if (post == null) { throw new ArgumentNullException(nameof(post)); }
+
+            _context.Update(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeletePostAsync(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            if (post == null) { throw new ArgumentNullException(nameof(post)); }
+
+            _context.Posts.Remove(post);
+            await _context.SaveChangesAsync();
+        }
     }
 }
